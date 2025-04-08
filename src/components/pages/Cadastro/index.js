f6bb01230946189289a66d4ac2131414653ba20e
Jeff <jefferson.lopes@germinare.org.br>
2025-04-08 08:48:20 -0300

    verificando se deu minha style bixho

--- src/components/pages/Cadastro/index.js
+++ src/components/pages/Cadastro/index.js
@@ -7,10 +7,10 @@ function Cadastro (){
             <form className={style.form}>
                 <h1>Cadastro</h1>
                 
-                <input type="text" placeholder="Nome Completo" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <input type="password" placeholder="Confirme sua senha" />
+                <input type="text" placeholder="Nome Completo" className="entrada"/>
+                <input type="email" placeholder="Email"  className="entrada" />
+                <input type="password" placeholder="Senha"  className="entrada"/>
+                <input type="password" placeholder="Confirme sua senha"  className="entrada"/>
                 <button type="submit">Cadastrar</button>
             </form>
         </>
